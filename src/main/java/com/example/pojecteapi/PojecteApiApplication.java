9d7d7a779c3cc5d80016b09e0aa8cbb060174e9d
david <drosal@jaumebalmes.net>
2022-10-03 17:26:30 +0200

    Initial commit

---
+++ src/main/java/com/example/pojecteapi/PojecteApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pojecteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojecteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PojecteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pojecteapi/PojecteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pojecteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojecteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
