9318d9d1d98384d2bcdfa3024b43cd159a1fe9b6
david <drosal@jaumebalmes.net>
2022-10-04 16:27:39 +0200

    llistat d'alumnes

---
+++ src/main/java/com/example/pojecteapi/AlumnesController.java
@@ -0,0 +1,17 @@
+package com.example.pojecteapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class AlumnesController {
+    @GetMapping("/alumnes")
+    public ArrayList<Alumnes> llistatAlumnes(){
+        ArrayList<Alumnes> a = new ArrayList<>();
+        a.add(new Alumnes("David","Rosal", "DAM"));
+        a.add(new Alumnes("Waneta","Bytheway","DAW"));
+        return a;
+    }
+}
