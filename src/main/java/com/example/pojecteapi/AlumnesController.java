08a327d2e4e928a29ee35f8409ac1ca3b800baef
david <drosal@jaumebalmes.net>
2022-10-07 16:55:01 +0200

    añadido llistatAlumnesGrupModulFaltes

--- src/main/java/com/example/pojecteapi/AlumnesController.java
+++ src/main/java/com/example/pojecteapi/AlumnesController.java
@@ -25,4 +25,17 @@ public class AlumnesController {
         a.add(new AlumnesModFaltes("Pau", "Chacon", "M13", "24%"));
         return a;
     }
+
+    @GetMapping("alumnes/grup/modul/faltes")
+    public ArrayList<AlumnesGrupModFaltes> llistatAlGrupModFal(){
+        ArrayList<AlumnesGrupModFaltes> a = new ArrayList<>();
+        //si grup o modul no valid error controlat
+        a.add(new AlumnesGrupModFaltes("David", "Rosal","DAM", "M01", "17%"));
+        a.add(new AlumnesGrupModFaltes("Marco", "Laureano", "DAW", "M02", "15%"));
+        a.add(new AlumnesGrupModFaltes("Marco", "Laureano","ASIX", "M10", "20%"));
+        a.add(new AlumnesGrupModFaltes("Pau", "Chacon","DAM", "M13", "24%"));
+        return a;
+
+    }
+
 }
