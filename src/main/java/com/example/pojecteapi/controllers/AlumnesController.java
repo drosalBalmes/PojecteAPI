8cc91074f36de69b31153c01ad7f07d93e2b80f5
david <drosal@jaumebalmes.net>
2022-10-20 16:50:41 +0200

    Alumnes/grup/{grup} (mejorable seguramente)

--- src/main/java/com/example/pojecteapi/controllers/AlumnesController.java
+++ src/main/java/com/example/pojecteapi/controllers/AlumnesController.java
@@ -18,63 +18,64 @@ import java.util.List;
 public class AlumnesController {
 	@Autowired //per instanciar automaticament cuan sigui necesari
 	AlumnesRepository alumnRep;
-	 ArrayList<Alumnes> a = new ArrayList<>();
-    @GetMapping("")
-    public List<Alumnes> llistatAlumnes(){
+
+	@GetMapping("")
+	public List<Alumnes> llistatAlumnes() {
 		return alumnRep.findAll();
-    }
+	}
 
-   ArrayList <AlumnesGrup> y = new ArrayList<>();
-    @GetMapping("/grup")
-    public ArrayList<AlumnesGrup> llistatAlumnesGrup(){
-    	y = new ArrayList<>();
-    	y.add(new AlumnesGrup("David","Rosal", "DAM"));
-        y.add(new AlumnesGrup("Waneta","Bytheway","DAW"));
-        y.add(new AlumnesGrup("Marco","Laureano","DAM"));
-        y.add(new AlumnesGrup("Lote","Gatorate","DAW"));
-        y.add(new AlumnesGrup("A","C","DAM"));
-        y.add(new AlumnesGrup("B","D","DAM"));
-    	return y;
-    }
-    //Variable de "/alumnes/grup".
-    @GetMapping("/grup/{grup}")
-    public ArrayList<AlumnesGrup> alumnesgrup(@PathVariable(value = "grup")String grup){
-    	ArrayList <AlumnesGrup> g = new ArrayList<>();
-    	for(AlumnesGrup x:y) {
-    		if(x.getGrup().equalsIgnoreCase(grup)) {
-    			g.add(x);
-    		}
-    	}
-    	return y;
-		}
-    
+	ArrayList<AlumnesGrup> y = new ArrayList<>();
 
-    @GetMapping("/modul/faltes")
-    public ArrayList<AlumnesModFaltes> llistatAlModFal(){
-    	//trucada al calcul de faltes
-    	ArrayList<AlumnesModFaltes> a = new ArrayList<>();
-    	a.add(new AlumnesModFaltes("David", "Rosal", "M01", "17%"));
-    	a.add(new AlumnesModFaltes("Marco", "Laureano", "M02", "15%"));
-    	a.add(new AlumnesModFaltes("Marco", "Laureano", "M10", "20%"));
-    	a.add(new AlumnesModFaltes("Pau", "Chacon", "M13", "24%"));
-    	return a;
-    }
+	@GetMapping("/grup")
+	public ArrayList<AlumnesGrup> llistatAlumnesGrup() {
+		y = new ArrayList<>();
+		y.add(new AlumnesGrup("David", "Rosal", "DAM"));
+		y.add(new AlumnesGrup("Waneta", "Bytheway", "DAW"));
+		y.add(new AlumnesGrup("Marco", "Laureano", "DAM"));
+		y.add(new AlumnesGrup("Lote", "Gatorate", "DAW"));
+		y.add(new AlumnesGrup("A", "C", "DAM"));
+		y.add(new AlumnesGrup("B", "D", "DAM"));
+		return y;
+	}
+	//Variable de "/alumnes/grup".
+//    @GetMapping("/grup/{grup}")
+//    public ArrayList<AlumnesGrup> alumnesgrup(@PathVariable(value = "grup")String grup){
+//    	ArrayList <AlumnesGrup> g = new ArrayList<>();
+//    	for(AlumnesGrup x:y) {
+//    		if(x.getGrup().equalsIgnoreCase(grup)) {
+//    			g.add(x);
+//    		}
+//    	}
+//    	return y;
+//		}
 
-    @GetMapping("/grup/modul/faltes")
-    public ArrayList<AlumnesGrupModFaltes> llistatAlGrupModFal(){
-    	//trucada al calcul de faltes
-    	ArrayList<AlumnesGrupModFaltes> a = new ArrayList<>();
-    	//si grup o modul no valid error controlat
-    	a.add(new AlumnesGrupModFaltes("David", "Rosal","DAM", "M01", "17%"));
-    	a.add(new AlumnesGrupModFaltes("Marco", "Laureano", "DAW", "M02", "15%"));
-    	a.add(new AlumnesGrupModFaltes("Marco", "Laureano","ASIX", "M10", "20%"));
-    	a.add(new AlumnesGrupModFaltes("Pau", "Chacon","DAM", "M13", "24%"));
-    	return a;
 
-    }
+	@GetMapping("/modul/faltes")
+	public ArrayList<AlumnesModFaltes> llistatAlModFal() {
+		//trucada al calcul de faltes
+		ArrayList<AlumnesModFaltes> a = new ArrayList<>();
+		a.add(new AlumnesModFaltes("David", "Rosal", "M01", "17%"));
+		a.add(new AlumnesModFaltes("Marco", "Laureano", "M02", "15%"));
+		a.add(new AlumnesModFaltes("Marco", "Laureano", "M10", "20%"));
+		a.add(new AlumnesModFaltes("Pau", "Chacon", "M13", "24%"));
+		return a;
+	}
 
-    @GetMapping("{id}")
-    public Alumnes llistatAlumnesId(@PathVariable Long id) {
+	@GetMapping("/grup/modul/faltes")
+	public ArrayList<AlumnesGrupModFaltes> llistatAlGrupModFal() {
+		//trucada al calcul de faltes
+		ArrayList<AlumnesGrupModFaltes> a = new ArrayList<>();
+		//si grup o modul no valid error controlat
+		a.add(new AlumnesGrupModFaltes("David", "Rosal", "DAM", "M01", "17%"));
+		a.add(new AlumnesGrupModFaltes("Marco", "Laureano", "DAW", "M02", "15%"));
+		a.add(new AlumnesGrupModFaltes("Marco", "Laureano", "ASIX", "M10", "20%"));
+		a.add(new AlumnesGrupModFaltes("Pau", "Chacon", "DAM", "M13", "24%"));
+		return a;
+
+	}
+
+	@GetMapping("{id}")
+	public Alumnes llistatAlumnesId(@PathVariable Long id) {
 		if (alumnRep.findById(id).isPresent()) {
 
 			return alumnRep.findById(id).get();
@@ -82,4 +83,23 @@ public class AlumnesController {
 			return null;
 		}
 	}
+
+	@GetMapping("/grup/{grup}")
+	public Iterable<Alumnes> alumnesgrup(@PathVariable String grup) {
+		List<Alumnes> llistatAlumnes = llistatAlumnes();
+		long id;
+		List<Long> i = new ArrayList<>();
+		int j = 0;
+		for (Alumnes a : llistatAlumnes) {
+			if ((a.getCicle() + a.getCurs()).equals(grup)) {
+				id = a.getId();
+				if (alumnRep.findById(id).isPresent()) {
+					i.add(a.getId());
+					j++;
+				}
+			}
+		}
+		return alumnRep.findAllById(i);
+	}
+
 }
